62804f5358429b7288f596030e280b8af3c66c91
Jéssica Sousa <jhessymary26@gmail.com>
2026-05-06 10:50:03 -0300

    Commit inicial - base do projeto

---
+++ finopt/src/main/java/com/elafinance/finopt/FinoptApplication.java
@@ -0,0 +1,13 @@
+package com.elafinance.finopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinoptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinoptApplication.class, args);
+	}
+
+}
